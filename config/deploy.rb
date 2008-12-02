9229f2751d216f43f704042067d8e2429770f915
padraic <padraicmcgee@gmail.com>
2008-12-02 07:39:57 -0600

    git deploy script, database backup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,87 @@
-set :stages, %w(staging production)
-set :default_stage, "production"
-require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
+require 'yaml'
 
+#############################################################
+#	Application
+#############################################################
+
+set :application, "www2"
+
+set :deploy_to, "/var/www/#{application}"
+set :user, "deploy"
+set :domain, "www2.sustainablewebsites.com"
+
+server domain, :app, :web
+role :db, domain, :primary => true
+set :runner, user
+set :keep_releases, 5
+
+default_run_options[:pty] = true
+
+#############################################################
+#	Staging Support
+#############################################################
+
+# set :stages, %w(staging production)
+# set :default_stage, "production"
+# require File.expand_path("#{File.dirname(__FILE__)}/../vendor/gems/capistrano-ext-1.2.1/lib/capistrano/ext/multistage")
+
+
+#############################################################
+#	Tasks
+#############################################################
+
+before :deploy, "db:backup"
+after :deploy, "passenger:restart"
+after "deploy:update_code","deploy:symlink_configs"
+
+
+# desc "Backup the database before running migrations"
+# task :before_migrate do 
+#   backup
+# end
+
+#############################################################
+#	Repository
+#############################################################
+
+#set :repository,  "git@sw.unfuddle.com:sw/www2.git"
+set :scm, "git"
+set :repository, "ssh://git@sw.unfuddle.com/sw/www2.git"
+set :git_shallow_clone, 1
+set :short_branch, "master"
+set :ssh_options, { :forward_agent => true, :port => 28822 }
+
+#############################################################
+#	Passenger
+#############################################################
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+#############################################################
+#	Shared
+#############################################################
+
+namespace(:deploy) do
+ task :symlink_configs, :roles => :app, :except => {:no_symlink => true} do
+   run <<-CMD
+     cd #{release_path} &&
+     ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
+   CMD
+ end
+
+ desc "restart override"
+ task :restart, :roles => :app do
+ end
+end
+
+#############################################################
+#	Database Tasks
+#############################################################
 
 namespace :db do
   desc 'Dumps the production database to db/production_data.sql on the remote server'
@@ -34,4 +114,18 @@ namespace :db do
     remote_db_download
     remote_db_cleanup
   end
+  
+  desc "Backup the remote production database"
+  task :backup, :roles => :db, :only => {:primary => true} do
+    filename = "#{application}.dump.#{Time.now.to_i}.sql.bz2"
+    file = "/var/backups/#{application}/#{filename}"
+    on_rollback { delete file }
+    #db = YAML::load(ERB.new(IO.read(File.join(File.dirname(__FILE__), 'database.yml'))).result)['production']
+    #run "mysqldump -u #{db['username']} --password=#{db['password']} #{db['database']} | bzip2 -c > #{file}"  do |ch, stream, data|
+    run "mysqldump -u root www2_sw_production | bzip2 -c > #{file}"  do |ch, stream, data|
+      puts data
+    end
+  end
+  
 end
+
