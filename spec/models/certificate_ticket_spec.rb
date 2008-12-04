d70025c6a441b8ae53e57e4166ee4059d9603d9e
padraic <padraicmcgee@gmail.com>
2008-12-04 08:30:16 -0600

    certificate ticket

--- spec/models/certificate_ticket_spec.rb
+++ spec/models/certificate_ticket_spec.rb
@@ -3,6 +3,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe CertificateTicket do
   before(:each) do
     @valid_attributes = {
+      :email => "test@example.com",
+      :host => "www.babelfish.com", 
+      :country => "US", 
+      :state => "MN", 
+      :city => "Minneapolis", 
+      :company_name => "Evil Inc.", 
+      :company_division => "main", 
+      :password => "pwd"
     }
   end
 
