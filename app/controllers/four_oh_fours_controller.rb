c407eb8f92be35c6d8dbda065dcfa5c0688e59a8
Ivan Storck <ivanoats@gmail.com>
2008-12-16 16:54:59 -0800

    test redirect match in 404 controller

--- app/controllers/four_oh_fours_controller.rb
+++ app/controllers/four_oh_fours_controller.rb
@@ -3,6 +3,10 @@ class FourOhFoursController < ApplicationController
   def index
     cleaned_path = request.path.gsub(/\//," ").strip
 
+    #checked cleaned_path against list of redirects in database
+    logger.info("CLEANED PATH: " + cleaned_path)
+    redirect_to  'https://billing.sustainablewebsites.com/step_one.php?gid=2' and return if cleaned_path.strip == 'cabn'
+
     #fake /:page_permalink
     @page = Page.find_by_permalink(cleaned_path)
     if @page
