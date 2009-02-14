77d889b48f65bdc863b77a21a6708ef56ae7ced4
padraic <padraicmcgee@gmail.com>
2009-02-13 20:53:48 -0600

    slightly better pages controller and tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,7 +2,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PagesController do
   fixtures :users, :pages, :roles, :roles_users
-
+  
+  def mock_page(stubs={})
+    @page ||= mock_model(Page, stubs)
+  end
+  
   describe "Anybody" do
 
     it "should show a page" do
@@ -25,17 +29,34 @@ describe PagesController do
   end
   
   describe "Administrator" do
-    before :each do 
-      user = users(:quentin)
-      
-      #user.show_receive(:roles).and_return([Role.administrator])
-      session[:user_id] = user.id
+    before :each do
+      login_as(:quentin)
     end
     
-    it "should show all pages" do
-      get 'index'
-      response.should be_success
+    describe "responding to GET index" do
+
+      it "should expose all pages as @pages" do
+        Page.should_receive(:find).with(:all).and_return([mock_page])
+        get :index
+        assigns[:pages].should == [mock_page]
+      end
+
     end
+    
+    describe "responding to DELETE destroy" do
+
+       it "should destroy the requested page" do
+         Page.should_receive(:find).with("37").and_return(mock_page)
+         mock_page.should_receive(:destroy)
+         delete :destroy, :id => "37"
+       end
+
+       it "should redirect to the pages list" do
+         Page.stub!(:find).and_return(mock_page(:destroy => true))
+         delete :destroy, :id => "1"
+         response.should redirect_to(pages_url)
+       end
+     end
     
   end
   
