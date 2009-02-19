b98baf7ac7e56b01455873fae077f37fcee32575
Padraic McGee <padraic@MacBookPro.local>
2009-02-19 01:32:50 -0600

    css fix, and very simple email validation for cert ticket + test

--- spec/models/certificate_ticket_spec.rb
+++ spec/models/certificate_ticket_spec.rb
@@ -17,4 +17,17 @@ describe CertificateTicket do
   it "should create a new instance given valid attributes" do
     CertificateTicket.create!(@valid_attributes)
   end
+  
+  it "should test for basic format of an email address without getting too trixie" do
+
+    CertificateTicket.new(@valid_attributes.merge(:email => 'a@b.c')).should be_valid
+    CertificateTicket.new(@valid_attributes.merge(:email => 'a+b@b.c')).should be_valid
+    CertificateTicket.new(@valid_attributes.merge(:email => 'abcdefghijk@b.c.e.f.g')).should be_valid
+
+    CertificateTicket.new(@valid_attributes.merge(:email => '@b.c')).should_not be_valid
+    CertificateTicket.new(@valid_attributes.merge(:email => 'a@b')).should_not be_valid
+    CertificateTicket.new(@valid_attributes.merge(:email => 'a.c')).should_not be_valid
+    CertificateTicket.new(@valid_attributes.merge(:email => 'a')).should_not be_valid
+
+  end
 end
