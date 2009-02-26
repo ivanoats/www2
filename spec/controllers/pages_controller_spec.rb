1fc460dedf60cbbb37768cc566fe5d582cfe7315
padraic <padraicmcgee@gmail.com>
2009-02-26 16:26:56 -0600

    fix for page update with error, page tests for update

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -57,6 +57,54 @@ describe PagesController do
          response.should redirect_to(pages_url)
        end
      end
+     
+     describe "responding to PUT update" do
+
+       describe "with valid params" do
+
+         it "should update the requested page" do
+           Page.should_receive(:find).with("37").and_return(mock_page)
+           mock_page.should_receive(:update_attributes).with({'these' => 'params'})
+           put :update, :id => "37", :page => {:these => 'params'}
+         end
+
+         it "should expose the requested page as @page" do
+           Page.stub!(:find).and_return(mock_page(:update_attributes => true))
+           put :update, :id => "1"
+           assigns(:page).should equal(mock_page)
+         end
+
+         it "should redirect to the page" do
+           Page.stub!(:find).and_return(mock_page(:update_attributes => true))
+           put :update, :id => "1"
+           response.should redirect_to(pages_url)
+         end
+
+       end
+
+       describe "with invalid params" do
+
+         it "should update the requested page" do
+           Page.should_receive(:find).with("37").and_return(mock_page)
+           mock_page.should_receive(:update_attributes).with({'these' => 'params'})
+           put :update, :id => "37", :page => {:these => 'params'}
+         end
+
+         it "should expose the page as @page" do
+           Page.stub!(:find).and_return(mock_page(:update_attributes => false))
+           put :update, :id => "1"
+           assigns(:page).should equal(mock_page)
+         end
+
+         it "should re-render the 'edit' template" do
+           Page.stub!(:find).and_return(mock_page(:update_attributes => false))
+           put :update, :id => "1"
+           response.should render_template('edit')
+         end
+
+       end
+
+     end
     
   end
   
