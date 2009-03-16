b4ca6ea8335ede42babbd688374899ecb07495b0
Ivan Storck <ivanoats@gmail.com>
2009-03-15 19:27:06 -0700

    changed domain helper

--- app/helpers/domain_helper.rb
+++ app/helpers/domain_helper.rb
@@ -4,12 +4,7 @@ module DomainHelper
   # gem is here: git://github.com/mlightner/universal_ruby_whois.git
   # also http://agilewebdevelopment.com/plugins/universal_whois_client
   # requires the dig and whois commands installed on the server
-  
-    
-    def nameservers(domain_name)
-      dig_output = `dig #{domain_name} NS +short`
-      dig_output.split
-    end
+   module ClassMethods
     
     def expiration_date(domain_name)
       Whois.find(domain_name).expiration_date
@@ -25,6 +20,32 @@ module DomainHelper
     
     def registered?(domain_name)
       Whois.find(domain_name).registered?
+    end
+  
+    def nameservers(domain_name)
+      dig_output = `dig #{domain_name} NS +short`
+      dig_output.split
+    end
+   
+    def a_record(domain_name)
+      `dig #{domain_name} A +short`.split
+    end
+    
+    def mx_records(domain_name)
+      `dig #{domain_name} MX +short`.split
+    end
+    
+  end
+  
+  module InstanceMethods
+    
+  end
+    
+    def self.included(base)
+      super
+      base.extend         ClassMethods
+#      receiver.extend         InstanceMethods
+#      receiver.send :include, InstanceMethods
     end
     
 end
