2b3359355f73a702d99f9e24c9896aea999175e3
J. Ryan Sobol <ryansobol.public@gmail.com>
2009-04-08 12:14:54 -0700

    improving product model validations and added hard-coded status and kind data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
-    validates_presence_of :name
-    validates_presence_of :cost
+  STATUS = %w(active disabled)
+  KINDS = %w(package add-on)
+  
+  validates_presence_of :name
+  validates_numericality_of :cost, :greater_than => 0, :only_integer => true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,8 +6,8 @@ describe Product do
       :name            => "Basic Web Hosting",
       :description     => "1,000MB Storage, 10,000MB Bandwidth, Unlimited Emails, Unlimited Addons, Fantastico",
       :cost            => 1000,
-      :recurring_month => 1,  #months
-      :status          => "active",  # [active|disabled]
+      :recurring_month => 1,
+      :status          => "active",
       :kind            => "package"
     }
   end
@@ -17,11 +17,24 @@ describe Product do
   end
   
   it "should require a name" do
-    lambda { Product.create!(@valid_attributes.reject{|key, value| key == :name }  ) }.should raise_error
+    lambda { Product.create!(@valid_attributes.merge(:name => '')) }.should raise_error
   end
   
-  it "should require a cost" do
-    lambda { Product.create!(@valid_attributes.reject{|key, value| key == :cost }  ) }.should raise_error
+  it "should only allow cost as integer" do
+    lambda { Product.create!(@valid_attributes.merge(:cost => '')) }.should raise_error
+    lambda { Product.create!(@valid_attributes.merge(:cost => 0.99)) }.should raise_error
+    lambda { Product.create!(@valid_attributes.merge(:cost => 1)) }.should_not raise_error
   end
-
+  
+  it "should only allow cost greater than 0" do
+    lambda { Product.create!(@valid_attributes.merge(:cost => 0)) }.should raise_error
+  end
+    
+  it "should have a list of status" do
+    Product::STATUS.should == %w(active disabled)
+  end
+  
+  it "should have a list of kinds" do
+    Product::KINDS.should == %w(package add-on)
+  end
 end
