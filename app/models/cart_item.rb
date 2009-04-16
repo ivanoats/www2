1587ae8cbeb8a41a994f999e19fdebbec661e22e
J. Ryan Sobol <ryansobol.public@gmail.com>
2009-04-16 09:19:20 -0700

    minor refactor to CartItem validations

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,7 +1,7 @@
 class CartItem < ActiveRecord::Base
   validates_presence_of :description
-  validates_numericality_of :unit_price_in_cents, :greater_than_or_equal_to => 0, :only_integer => true
-  validates_numericality_of :quantity, :greater_than => 0, :only_integer => true
+  validates_numericality_of :unit_price_in_cents, :only_integer => true, :greater_than_or_equal_to => 0
+  validates_numericality_of :quantity, :only_integer => true, :greater_than => 0
   
   belongs_to :cart
   has_one :product
