5d3992d41b1768fe07e5378988e754f2e16d9089
padraic <padraicmcgee@gmail.com>
2009-04-20 00:01:50 -0500

    removed service merchant

--- vendor/plugins/service_merchant/recurring_billing/lib/am_extensions.rb
+++
@@ -1,1 +0,0 @@
-Dir[File.dirname(__FILE__) + '/am_extensions/*.rb'].each{|g| require g}
--- vendor/plugins/service_merchant/subscription_management/lib/models/subscription_profile.rb
+++
@@ -1,4 +0,0 @@
-class SubscriptionProfile < ActiveRecord::Base
-belongs_to :subscription
-belongs_to :recurring_payment_profile
-end
