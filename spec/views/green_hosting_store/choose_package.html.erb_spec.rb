5b8b3c47435c8a519012fb64b237f02e3accfc45
J. Ryan Sobol <ryansobol.public@gmail.com>
2009-04-29 15:26:03 -0700

    Removed pending view test code (the rational is Ivan is considering Cucumber t ofully test Model - interaction)

--- spec/views/green_hosting_store/choose_package.html.erb_spec.rb
+++ spec/views/green_hosting_store/choose_package.html.erb_spec.rb
@@ -2,12 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/green_hosting_store/choose_package" do
   before(:each) do
-    #render 'green_hosting_store/choose_package'
+    # Consider using Cucomber to fully test Model -> View interaction
+    # assigns[:cart] = Cart.new
+    # assigns[:package] = Product.new
+    # assigns[:packages] = [[ Product.new, Product.new ]]
+    # render 'green_hosting_store/choose_package'
   end
-
-  it "should use 50/50 2 column layout"
-  
-  it "should render the cart"
-  
-  it "should render package choices form"
 end
