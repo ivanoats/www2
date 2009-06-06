72db8ffddfda05d6b4dffed45b8233d3d5910faf
padraic <padraicmcgee@gmail.com>
2009-06-06 18:28:43 -0500

    clone staging to production

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,16 @@
 set :deploy_to, "/var/www/#{application}_staging"
+
+set :production_deploy_to, "/var/www/#{application}"
+
+namespace :copy do
+  desc "Clone Production Environment to Staging"
+  task :production do
+    run "cp -R #{production_deploy_to}/shared/assets/* #{deploy_to}/shared/assets/"
+    
+    filename = "www2_sw_staging.sql"
+    file = "/var/backups/#{application}/#{filename}"
+    run "mysqldump -u root www2_sw_production > #{file}"
+    run "mysql -u root www2_sw_staging < #{file}"
+    
+  end
+end
