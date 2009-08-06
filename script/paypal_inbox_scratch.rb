d00f67504ec8f3494b4a8cf518c904a08405fce7
Ivan Storck <ivanoats@gmail.com>
2009-08-06 16:55:45 -0700

    still working on paypal payments

--- script/paypal_inbox_scratch.rb
+++ script/paypal_inbox_scratch.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'net/pop'
-require File.dirname(__FILE__) + '/../config/environment'
+#require File.dirname(__FILE__) + '/../config/environment'
 
 logger = RAILS_DEFAULT_LOGGER
 
@@ -25,9 +25,28 @@ amount =  find_after_until(email_body,"Amount: $"," ").to_i
 subscription_number = find_after_until(email_body,"Subscription Number: ","\n")
 #puts find_after_until(email_body,"Subscription Name: ", "\n")
 domain_name = find_after_until(find_after_until(email_body,"Subscription Name: ", "\n"), "[","]")
+user_email = find_after_until(email_body,"Email: ","\n")
 
+puts "Received from PayPal on date: " + email_parsed.date.strftime('%Y-%m-%d')
+puts "unix date: " + email_parsed.date.to_i.to_s
 puts "Amount is: " + amount.to_s
 puts "Subscription number is: " + subscription_number
 puts "Domain name is: " + domain_name
+puts "Email is: " + user_email
+
+# find by txn_id (subscription number)
+order = Whmaphostingorder.find_by_txn_id(subscription_number)
+if order.nil?
+  order = Whmaphostingorder.find_by_domain_name(domain_name)
+end
+if order.nil?
+  order = Whmaphostingorder.find_by_email(user_email)
+end
+if order.nil?
+  puts "failed to find any info in email"
+end
+
+pp order
+
 
 logger.info "Finished Mail Importer Test."
