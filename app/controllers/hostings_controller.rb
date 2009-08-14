7fa5e4f68c627550c3d4f60d447e7f8bd211d2d9
padraic <padraicmcgee@gmail.com>
2009-08-14 16:23:14 -0500

    project sync

--- app/controllers/hostings_controller.rb
+++ app/controllers/hostings_controller.rb
@@ -1,9 +1,26 @@
 class HostingsController < ApplicationController
+
+  layout 'account'
+  before_filter :login_required
+  before_filter :require_account
+  
   include ModelControllerMethods
-  before_filter :get_server
+  
+  def cancel
+    load_object
+    @hosting.delete!
+    flash[:notice] = 'Web hosting package cancelled'
+    redirect_to :controller => "account", :action => 'hosting'
+  end
   
 private
-  def get_server
-    @server = Server.find(params[:server_id])
+
+  def require_account
+    @account = Account.find_by_id(session[:account]) || current_user.accounts.first
+    redirect_to root_url and flash[:error] = "An account is required to view this page" if @account.nil?
+  end
+
+  def scoper
+    @account.hostings
   end
 end
