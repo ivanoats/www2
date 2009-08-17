a26115b3d0df539fae9e190e2cce956f2bd0ec5b
padraic <padraicmcgee@gmail.com>
2009-08-17 04:17:25 -0500

    sortable and server / hosting listing

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,12 +1,26 @@
 class ServersController < ApplicationController
   include ModelControllerMethods
   
+  sortable_table Server
+  
   layout 'admin'
   require_role "Administrator"
   
   before_filter :login_required
   
+  def index
+    load_object
+
+
+    get_sorted_objects(params, :table_headings => [['Name', 'name'],
+    ['IP Address', 'ip_address'], ['Whm User','whm_user']])
+    
+    
+  end
+  
   def hostings
     load_object
+    @accounts = @server.whm.accounts
+    @packages = @server.whm.packages    
   end
 end
