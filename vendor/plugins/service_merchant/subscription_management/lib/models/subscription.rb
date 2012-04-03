47df756709b86caa9ae7520eed6a6a451a87b1e9
Deploy <deploy@sustainablewebsites.com>
2012-04-03 03:48:53 +0000

    production 2012-04-02

--- vendor/plugins/service_merchant/subscription_management/lib/models/subscription.rb
+++
@@ -1,9 +0,0 @@
-class Subscription < ActiveRecord::Base
-  has_many :subscription_profiles
-  has_many :recurring_payment_profiles, :through => :subscription_profiles
-
-  # Returns billing amount (sum of net amount + taxes) 
-  def billing_amount
-    self.net_amount + self.taxes_amount
-  end
-end
